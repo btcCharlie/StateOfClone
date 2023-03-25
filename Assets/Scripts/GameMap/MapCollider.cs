2c414d81fed6d2389e7372fda8f9ed898bbb9940
Thomas B. Charlie <thomas.b.charlie@gmail.com>
2023-03-25 22:48:25 +0100

    MapCollider can now generate the simplified mesh

--- Assets/Scripts/GameMap/MapCollider.cs
+++ Assets/Scripts/GameMap/MapCollider.cs
@@ -6,18 +6,24 @@ namespace StateOfClone.GameMap
     [RequireComponent(typeof(MeshCollider), typeof(MeshFilter))]
     public class MapCollider : MonoBehaviour
     {
-        private Mesh _colliderMesh;
-        private MeshCollider _collider;
+        private MeshCollider _meshCollider;
+
+        private void Awake()
+        {
+            _meshCollider = GetComponent<MeshCollider>();
+        }
 
         private void Start()
         {
-            Mesh originalMesh = GetComponent<MeshFilter>().sharedMesh;
-
         }
 
-        public void SimplifyMesh()
+        public void GenerateColliderMesh()
         {
-            Mesh simplifiedMesh = MeshUtils.MergeVertices(_colliderMesh);
+            Mesh colliderMesh = MeshUtils.MergeVertices(
+                GetComponent<MeshFilter>().sharedMesh
+                );
+            colliderMesh.name = "Collider Mesh";
+            _meshCollider.sharedMesh = colliderMesh;
         }
     }
 }
