d0dba75c72dffa78c350b2bc16987792bba81146
Thomas B. Charlie <thomas.b.charlie@gmail.com>
2023-03-26 19:52:57 +0200

    merging vertices in mesh simplification algorithm in MeshUtils is now two orders of magnitude faster thanks to using hash table comparisons instead of iterating across all vertices (courtesy of chatGPT) also, removed the unused map collider script

--- Assets/Scripts/Core/MeshUtils.cs
+++ Assets/Scripts/Core/MeshUtils.cs
@@ -1,9 +1,17 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace StateOfClone.Core
 {
     public static class MeshUtils
     {
+        /// <summary>
+        /// Merges vertices in the given mesh that occupy the same position within the specified tolerance.
+        /// Returns a new mesh with the merged vertices and reduced number of vertices.
+        /// </summary>
+        /// <param name="mesh">The mesh to merge vertices in.</param>
+        /// <param name="tolerance">The tolerance within which vertices are considered to occupy the same position.</param>
+        /// <returns>A new mesh with merged vertices.</returns>
         public static Mesh MergeVertices(Mesh mesh, float tolerance = 0.001f)
         {
             Vector3[] vertices = mesh.vertices;
@@ -19,67 +27,67 @@ namespace StateOfClone.Core
             int[] newTriangles = MapTriangles(triangles, vertexMap);
 
             // Create a new mesh with the merged vertices and triangles
-            Mesh newMesh = new();
+            Mesh newMesh = new Mesh();
             newMesh.vertices = mergedVertices;
             newMesh.triangles = newTriangles;
 
-            Debug.Log(
-                $@"Mesh vertex count reduced from {vertices.Length} to 
-                {mergedVertices.Length} vertices. Reduction of 
-                {(float)mergedVertices.Length / vertices.Length:P2}"
-                );
+            // Debug.Log($"Mesh vertex count reduced from {vertices.Length} to {mergedVertices.Length} vertices. Reduction of {(float)mergedVertices.Length / vertices.Length:P2}");
 
             return newMesh;
         }
 
         private static Vector3[] MergeVertices(Vector3[] vertices, float tolerance)
         {
-            Vector3[] mergedVertices = new Vector3[vertices.Length];
-            int newVertexIndex = 0;
+            var hash = new Dictionary<Vector3, int>();
+            var mergedVertices = new List<Vector3>(vertices.Length);
 
             for (int originalVertexIndex = 0; originalVertexIndex < vertices.Length; originalVertexIndex++)
             {
-                bool merged = false;
+                var originalVertex = vertices[originalVertexIndex];
+                var roundedVertex = new Vector3(
+                    Mathf.Round(originalVertex.x / tolerance) * tolerance,
+                    Mathf.Round(originalVertex.y / tolerance) * tolerance,
+                    Mathf.Round(originalVertex.z / tolerance) * tolerance
+                    );
 
-                for (int mergedVertexIndex = 0; mergedVertexIndex < newVertexIndex; mergedVertexIndex++)
+                if (hash.TryGetValue(roundedVertex, out int mergedVertexIndex))
                 {
-                    if (Vector3.Distance(mergedVertices[mergedVertexIndex], vertices[originalVertexIndex]) < tolerance)
-                    {
-                        merged = true;
-                        break;
-                    }
+                    // Vertex is already merged
                 }
-
-                if (!merged)
+                else
                 {
-                    mergedVertices[newVertexIndex] = vertices[originalVertexIndex];
-                    newVertexIndex++;
+                    mergedVertexIndex = mergedVertices.Count;
+                    mergedVertices.Add(originalVertex);
+                    hash[roundedVertex] = mergedVertexIndex;
                 }
             }
 
-            Vector3[] result = new Vector3[newVertexIndex];
-            for (int i = 0; i < newVertexIndex; i++)
-            {
-                result[i] = mergedVertices[i];
-            }
-
-            return result;
+            return mergedVertices.ToArray();
         }
 
         private static int[] MapVertices(Vector3[] originalVertices, Vector3[] mergedVertices, float tolerance)
         {
-            int[] vertexMap = new int[originalVertices.Length];
+            var hash = new Dictionary<Vector3, int>();
+            var vertexMap = new int[originalVertices.Length];
+
+            for (int i = 0; i < mergedVertices.Length; i++)
+            {
+                var mergedVertex = mergedVertices[i];
+                var roundedVertex = new Vector3(Mathf.Round(mergedVertex.x / tolerance) * tolerance,
+                                                Mathf.Round(mergedVertex.y / tolerance) * tolerance,
+                                                Mathf.Round(mergedVertex.z / tolerance) * tolerance);
+
+                hash[roundedVertex] = i;
+            }
 
             for (int i = 0; i < originalVertices.Length; i++)
             {
-                for (int j = 0; j < mergedVertices.Length; j++)
-                {
-                    if (Vector3.Distance(originalVertices[i], mergedVertices[j]) < tolerance)
-                    {
-                        vertexMap[i] = j;
-                        break;
-                    }
-                }
+                var originalVertex = originalVertices[i];
+                var roundedVertex = new Vector3(Mathf.Round(originalVertex.x / tolerance) * tolerance,
+                                                Mathf.Round(originalVertex.y / tolerance) * tolerance,
+                                                Mathf.Round(originalVertex.z / tolerance) * tolerance);
+
+                vertexMap[i] = hash[roundedVertex];
             }
 
             return vertexMap;
@@ -87,7 +95,7 @@ namespace StateOfClone.Core
 
         private static int[] MapTriangles(int[] triangles, int[] vertexMap)
         {
-            int[] newTriangles = new int[triangles.Length];
+            var newTriangles = new int[triangles.Length];
 
             for (int i = 0; i < triangles.Length; i++)
             {
