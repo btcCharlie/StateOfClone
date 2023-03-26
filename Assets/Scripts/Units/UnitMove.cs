d163d9396e43a661279d40f3c645514b93f4a9fe
Thomas B. Charlie <thomas.b.charlie@gmail.com>
2023-03-26 19:05:35 +0200

    units now also rotate correctly when moving using rigidbodies

--- Assets/Scripts/Units/UnitMove.cs
+++ Assets/Scripts/Units/UnitMove.cs
@@ -23,6 +23,8 @@ namespace StateOfClone.Units
         private bool _isMoving = false;
         private List<Vector3> _path;
 
+        private Quaternion fromRotation, toRotation;
+
         private void Awake()
         {
             _playerInput = CustomInputManager.Instance.PlayerInput;
@@ -51,38 +53,62 @@ namespace StateOfClone.Units
                 return;
             }
 
+            float turnSpeedDegPerSec = 50f;
+
             if (_path.Count == 0)
             {
-                Debug.Log("Path is empty");
+                // reached destination
+                Debug.Log("Reached destination");
                 _isMoving = false;
                 return;
             }
 
             Vector3 currentWaypoint = _path[^1];
-            if (Vector3.Distance(transform.position, currentWaypoint) < 0.1f)
+            if (Vector3.Distance(transform.position, currentWaypoint) < 0.5f)
             {
+                // reached waypoint
+                Debug.Log("Reached waypoint");
                 _path.RemoveAt(_path.Count - 1);
                 return;
             }
 
             Vector3 direction = (currentWaypoint - transform.position).normalized;
-            _rigidbody.MovePosition(
-                transform.position + 10f * Time.fixedDeltaTime * direction
-                );
-        }
+            toRotation = Quaternion.LookRotation(direction, transform.up);
 
-        private void OnEnable()
-        {
-            _unitMoveAction.performed += OnUnitMove;
-        }
+            // Vector3 forwardFromRotation = transform.rotation * Vector3.forward;
+            // Vector3 forwardToRotation = toRotation * Vector3.forward;
+            // float angleFromRotation = Mathf.Atan2(forwardFromRotation.x, forwardFromRotation.z) * Mathf.Rad2Deg;
+            // float angleToRotation = Mathf.Atan2(forwardToRotation.x, forwardToRotation.z) * Mathf.Rad2Deg;
+            // float angleDiff = Mathf.DeltaAngle(angleFromRotation, angleToRotation);
 
-        private void OnDisable()
-        {
-            _unitMoveAction.performed -= OnUnitMove;
+            Vector3 toRotationEulers = toRotation.eulerAngles;
+            toRotationEulers.x = transform.rotation.eulerAngles.x;
+            toRotationEulers.z = transform.rotation.eulerAngles.z;
+            toRotation = Quaternion.Euler(toRotationEulers);
+
+            float angleToTurnDeg = Quaternion.Angle(transform.rotation, toRotation);
+
+            // tracked vehicle behavior - rotates in place and then moves
+            if (angleToTurnDeg > 1f)
+            {
+                float turnProgressPerSec = turnSpeedDegPerSec / angleToTurnDeg;
+
+                _rigidbody.MoveRotation(Quaternion.Slerp(
+                    transform.rotation,
+                    toRotation,
+                    turnProgressPerSec * Time.fixedDeltaTime));
+            }
+            else
+            {
+                _rigidbody.MovePosition(
+                    transform.position + 10f * Time.fixedDeltaTime * transform.forward
+                    );
+            }
         }
 
         private void OnDestroy()
         {
+            OnDeselected();
             _unit.OnSelected.RemoveListener(OnSelected);
             _unit.OnDeselected.RemoveListener(OnDeselected);
         }
@@ -90,10 +116,12 @@ namespace StateOfClone.Units
         private void OnSelected()
         {
             enabled = _isSelected = true;
+            _unitMoveAction.performed += OnUnitMove;
         }
 
         private void OnDeselected()
         {
+            _unitMoveAction.performed -= OnUnitMove;
             _isSelected = false;
         }
 
