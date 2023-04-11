ba098e796b82f113697f606a6c83f21fddba9406
Thomas B. Charlie <thomas.b.charlie@gmail.com>
2023-04-11 23:00:12 +0200

    seek now displays gizmos of its intentions

--- Assets/Scripts/Units/Movement/SteeringSeek.cs
+++ Assets/Scripts/Units/Movement/SteeringSeek.cs
@@ -4,10 +4,34 @@ namespace StateOfClone.Units
 {
     public class SteeringSeek : SteeringBehavior
     {
+        private Vector3 _desiredVelocity, _seekSteering;
+
         public override Vector3 GetSteering(Vector3 target)
         {
-            Vector3 desiredVelocity = (target - _rb.position).normalized * _unit.UnitData.MaxSpeed;
-            return (desiredVelocity - _rb.velocity) * 10f;
+            _desiredVelocity = Vector3.ClampMagnitude(
+                target - _rb.position, _unit.UnitData.MaxSpeed
+                );
+            _seekSteering = _desiredVelocity - _rb.velocity;
+            // _seekSteering *= 10f;
+
+
+            return _seekSteering;
+        }
+
+        private void OnDrawGizmos()
+        {
+            Color ogColor = Gizmos.color;
+
+            Vector3 offset = Vector3.up * 3f;
+            Vector3 from = transform.position + offset;
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(from, from + _desiredVelocity);
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(from, from + _seekSteering);
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(from, from + _rb.velocity);
+
+            Gizmos.color = ogColor;
         }
     }
 }
