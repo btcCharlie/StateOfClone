0edb510a4889eab4207ad5741ad65f71c3da4c73
btcharlie <thomas.b.charlie@gmail.com>
2023-07-11 18:13:37 +0200

    Tracked locomotion now has two limits for in-place turning - a lower limit for when it starts turning when stationary (to align itself better with the target) and an upper one that defines when the tank should stop and re-adjust itself in-place when already moving (to avoid constant re-jiggling enroute)

--- Assets/Scripts/Units/Movement/TrackedLocomotion.cs
+++ Assets/Scripts/Units/Movement/TrackedLocomotion.cs
@@ -4,15 +4,42 @@ namespace StateOfClone.Units
 {
     public class TrackedLocomotion : Locomotion
     {
-        [SerializeField] private float _inPlaceTurnAngleLimitDegrees = 30f;
+        [SerializeField] private float _inPlaceTurnLowerLimitDegrees = 5f;
+        [SerializeField] private float _inPlaceTurnUpperLimitDegrees = 15f;
+        private float _activeLimit;
 
-        protected override void ApplySteering(Vector3 newPosition, Quaternion newRotation)
+        protected override void Start()
         {
-            // Update the unit's position and rotation
-            if (Mathf.Abs(CurrentAngularSpeedDegPerSec) < _inPlaceTurnAngleLimitDegrees)
+            base.Start();
+
+            _activeLimit = _inPlaceTurnLowerLimitDegrees;
+
+        }
+
+        protected override void ApplySteering(
+            Vector3 newPosition, Quaternion newRotation
+            )
+        {
+            Debug.Log($"Active limit: {_activeLimit}");
+
+            if (Mathf.Abs(SteeringParams.Yaw) <= _activeLimit)
             {
                 _rb.position = newPosition;
             }
+            else
+            {
+                CurrentSpeedUnitPerSec = 0f;
+            }
+
+            if (Mathf.Abs(CurrentSpeedUnitPerSec) < 0.1f)
+            {
+                _activeLimit = _inPlaceTurnLowerLimitDegrees;
+            }
+            else
+            {
+                _activeLimit = _inPlaceTurnUpperLimitDegrees;
+            }
+
             _rb.rotation = newRotation;
         }
     }
